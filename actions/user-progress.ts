8640171bb5ec7acdbfd6e447a826e4989ed228bd
Martín Eduardo Aguayo <martineduardoaguayo@gmail.com>
2024-07-29 16:11:06 -0300

    19: SHOP

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -8,6 +8,9 @@ import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+
+export const POINTS_TO_REFILL = 50;
+
 export const upsertUserProgress = async (courseId: number) =>{
   const {userId} = auth();
   const user = await currentUser();
@@ -104,3 +107,29 @@ export const reduceHearts = async (challengeId: number) => {
   // revalidatePath(`/lesson/${lessonId}`);
 
 }
+
+export const refillHearts = async () => {
+  const currentUserProgress = await getUserProgress();
+
+  if(!currentUserProgress) {
+    throw new Error("User progress not found");
+  };
+
+  if(currentUserProgress.hearts === 5) {
+    throw new Error("Hearts are full");
+  };
+
+  if(currentUserProgress.points < POINTS_TO_REFILL) {
+    throw new Error("Not enough points");
+  };
+
+  await db.update(userProgress).set({
+    hearts: 5,
+    points: currentUserProgress.points - POINTS_TO_REFILL,
+  }).where(eq(userProgress.userId, currentUserProgress.userId));
+
+  revalidatePath("/learn");
+  revalidatePath("/quest");
+  revalidatePath("/leaderboard");
+  revalidatePath("/shop");
+};
---
+++ app/(main)/shop/page.tsx
@@ -0,0 +1,47 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import Items from "./items";
+
+const ShopPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <div className="w-full flex flex-col items-center">
+          <Image alt="Shop" src="/shop.svg" width={90} height={90} />
+          <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">
+            Shop
+          </h1>
+          <p className="text-neutral-500 text-center text-lg mb-6">
+            Spend your points on cool stuff.
+          </p>
+          <Items
+            hearts={userProgress.hearts}
+            points={userProgress.points}
+            hasActiveSubscription={false}
+          />
+        </div>
+      </FeedWrapper>
+    </div>
+  );
+};
+
+export default ShopPage;
